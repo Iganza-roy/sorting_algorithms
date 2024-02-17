9e65292224e3f80be166c45da660d82885937dbb
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-02-17 06:11:19 -0800

    mods

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,21 +7,21 @@
  * Return: nothing
  */
 
-void bubble_sort(int *array, size_t size);
+void bubble_sort(int *array, size_t size)
 {
-	int i, j, tmp;
+	unsigned long int i, j, tmp;
 
-	for (i = 0; i < size, i++)
+	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size - 1; j++)
+		for (j = 0; j < (size - 1); j++)
 		{
-			if array[j] > array[j + 1]
+			if (array[j] > array[j + 1])
 			{
 				tmp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
 			}
-			print_array(array, size)
 		}
+		print_array(array, size);
 	}
 }
--- sort.h
+++ sort.h
@@ -1,6 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stddef.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -19,7 +20,7 @@ typedef struct listint_s
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
+void bubble_sort(int *array, size_t size);
 
 
 #endif
