479c86ba3a3a42e4be204ff2806303b5388db526
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-02-20 10:42:59 -0800

    modified 100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -10,25 +10,25 @@
  */
 void shell_sort(int *array, size_t size)
 {
-    size_t gap = 1, i, j;
+    size_t increment = 1, i, j;
     int tmp;
 
     if (!array || size < 2)
         return;
 
-    while (gap < size / 3)
-        gap = gap * 3 + 1;
+    while (increment < size / 3)
+        increment = increment * 3 + 1;
 
-    while (gap >= 1)
+    while (increment >= 1)
     {
-        for (i = gap; i < size; i++)
+        for (i = increment; i < size; i++)
         {
             tmp = array[i];
-            for (j = i; j >= gap && array[j - gap] > tmp; j -= gap)
-                array[j] = array[j - gap];
+            for (j = i; j >= increment && array[j - increment] > tmp; j -= increment)
+                array[j] = array[j - increment];
             array[j] = tmp;
         }
-        gap = (gap - 1) / 3;
+        increment = (increment - 1) / 3;
         print_array(array, size);
     }
 }
