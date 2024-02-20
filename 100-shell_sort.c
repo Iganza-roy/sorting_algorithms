bd142524ee086780e2a6ea856a98ad9b25cf78ce
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-02-20 10:46:00 -0800

    modified betty coding style

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -10,25 +10,25 @@
  */
 void shell_sort(int *array, size_t size)
 {
-    size_t increment = 1, i, j;
-    int tmp;
+	size_t increment = 1, i, j;
+	int tmp;
 
-    if (!array || size < 2)
-        return;
+	if (!array || size < 2)
+		return;
 
-    while (increment < size / 3)
-        increment = increment * 3 + 1;
+	while (increment < size / 3)
+		increment = increment * 3 + 1;
 
-    while (increment >= 1)
-    {
-        for (i = increment; i < size; i++)
-        {
-            tmp = array[i];
-            for (j = i; j >= increment && array[j - increment] > tmp; j -= increment)
-                array[j] = array[j - increment];
-            array[j] = tmp;
-        }
-        increment = (increment - 1) / 3;
-        print_array(array, size);
-    }
+	while (increment >= 1)
+	{
+		for (i = increment; i < size; i++)
+		{
+			tmp = array[i];
+			for (j = i; j >= increment && array[j - increment] > tmp; j -= increment)
+				array[j] = array[j - increment];
+			array[j] = tmp;
+		}
+		increment = (increment - 1) / 3;
+		print_array(array, size);
+	}
 }
